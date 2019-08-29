1d143a11028af75e73a1bc20e95899ff723864c6
faversente <fabio.aversente@accenture.com>
2019-08-29 08:46:08 +0200

    Fixed url

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // TODO: Remove the cors-anywhere url on production
-const API_URL = 'https://j6fo38azgk.execute-api.eu-central-1.amazonaws.com/dev'
+const API_URL = 'https://cors-anywhere.herokuapp.com/https://j6fo38azgk.execute-api.eu-central-1.amazonaws.com/dev'
 
 const config = {
   baseURL: API_URL
@@ -9,22 +9,22 @@ const config = {
 
 export default {
   async fetchTodos () {
-    let response = await axios.get('/dev/tasks', config)
+    let response = await axios.get('/tasks', config)
     return response.data
   },
 
   async updateTodo (todo) {
-    return axios.put('/dev/tasks/' + todo.id, todo, config)
+    return axios.put('/tasks/' + todo.id, todo, config)
   },
 
   async createTodo (todo) {
-    let response = await axios.post('/dev/tasks', todo, config)
+    let response = await axios.post('/tasks', todo, config)
     let locationHeader = response.headers['location']
     let pathSegments = locationHeader.split('/')
     return pathSegments[pathSegments.length - 1]
   },
 
   async deleteTodo (id) {
-    return axios.delete('/dev/tasks/' + id, config)
+    return axios.delete('/tasks/' + id, config)
   }
 }
